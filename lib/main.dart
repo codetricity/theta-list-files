92df3095dc23998e39002e7c6f0844bed9ec0d8d
craig <craig@craigs-iMac.attlocal.net>
2020-03-24 22:30:39 -0700

    improved messages under buttons

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
 
   String message = "row";
-  String responseText = "Lorem ipsum dolor sit amet, consectetur "
-  "adipiscing elit, sed do eiusmod tempor incididunt "
-  "ut labore et dolore magna aliqua. Ut enim ad minim veniam, "
-  "quis nostrud exercitation ullamco laboris nisi ut aliquip "
-  "ex ea commodo consequat. Duis aute irure dolor in reprehenderit "
-  "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
-  "Excepteur sint occaecat cupidatat non proident, sunt in "
-  "culpa qui officia deserunt mollit anim id est laborum.";
+  String responseText = "";
 
 
   Future<http.Response> getTestData () async {
@@ -77,7 +70,7 @@ class _MyAppState extends State<MyApp> {
               onPressed: (() {
                 listFiles();
                 setState(() {
-                  message = "blue was pressed last";
+                  message = "Listing RICOH THETA Files";
                 });
 
                 print("pressed button");
@@ -89,7 +82,7 @@ class _MyAppState extends State<MyApp> {
               onPressed: (() {
                 getTestData();
                 setState(() {
-                  message = "red was pressed last";
+                  message = "showing test data";
                 });
               }
               ),
